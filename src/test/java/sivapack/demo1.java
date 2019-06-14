f8443dda25ee46f241d0bd90fa10a642f86bf59e
Training_C2a.05.08 <Training_C2a.05.08@CDC2-D-441SR6J.dir.svc.accenture.com>
2019-06-14 17:19:34 +0530

    report

--- src/test/java/sivapack/demo1.java
+++ src/test/java/sivapack/demo1.java
@@ -2,11 +2,14 @@ package sivapack;
 
 import org.junit.runner.RunWith;
 
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(
+		monochrome=true,
+		plugin= {"pretty","json:target/src/test/java/multipledemo/cucumber-reports.json"})
 
 public class demo1 {
 	
-
 }
